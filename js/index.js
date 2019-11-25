7ed65fe916129859350b1d5da619ee1717260404
Matthew <matthewjamesbedard@outlook.com>
2019-11-25 13:52:02 -0500

    mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,48 @@
 // Your code goes here
+
+// ! Mouse Over
+const mouseOver = document.querySelector(".intro img");
+// mouse over
+mouseOver.addEventListener("mouseover", () => {
+    mouseOver.style.border = "2px solid black";
+})
+// mouse leave
+mouseOver.addEventListener("mouseleave", () => {
+    mouseOver.style.border = "none";
+})
+
+// ****** END OF MOUSE OVER **********
+
+// ? Key Down
+
+
+// ! Wheel
+const wheel = document.querySelector(".img-content img");
+wheel.onwheel = "zoom";
+
+
+// ! drag / drop
+
+
+
+// ! load
+
+
+
+// !Focus
+
+
+
+// ! Resize 
+
+
+
+// ! scroll 
+
+
+
+// ! Select
+
+
+
+// !dblclick
