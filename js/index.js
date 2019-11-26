15193de069fd0f5307ff650dc108983cedb8bdf5
Matthew <matthewjamesbedard@outlook.com>
2019-11-26 11:35:01 -0500

    updated files for

--- js/index.js
+++ js/index.js
@@ -4,45 +4,46 @@
 const mouseOver = document.querySelector(".intro img");
 // mouse over
 mouseOver.addEventListener("mouseover", () => {
-    mouseOver.style.border = "2px solid black";
-})
+  mouseOver.style.border = "2px solid black";
+});
 // mouse leave
 mouseOver.addEventListener("mouseleave", () => {
-    mouseOver.style.border = "none";
-})
+  mouseOver.style.border = "none";
+});
 
 // ****** END OF MOUSE OVER **********
 
 // ? Key Down
-
+// const keyDown = documente.querySelector(".intro h2");
+// keyDown.addEventListener("keydown", logKey => {
+//   function logKey() {
+//     alert("Stop Messing Around on Here");
+//   }
+// });
 
 // ! Wheel
-const wheel = document.querySelector(".img-content img");
-wheel.onwheel = "zoom";
-
+// const wheel = document.querySelector(".img-content img");
+// wheel.onwheel = "zoom";
 
 // ! drag / drop
 
-
-
 // ! load
-
-
 
 // !Focus
 
+// ******* Resize
+window.addEventListener("resize", () => {
+  alert("put me back to my intended size");
+});
+// ********* end of resize *****************************
 
-
-// ! Resize 
-
-
-
-// ! scroll 
-
-
+// ! scroll
 
 // ! Select
 
-
-
 // !dblclick
+const double = document.querySelector(".main-navigation");
+double.addEventListener("dblclick", () => {
+  double.style.color = "purple";
+  double.classList.toggle;
+});
